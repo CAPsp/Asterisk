86039e52bc3cc425d8ff9c1ec17f9be93adad849
Hokoyama <k0416m0204@yahoo.co.jp>
2016-09-24 19:04:57 +0900

    ステージ数保持のオブジェクト作成、プレハブフォルダ作成

---
+++ Project/Assets/Scripts/Title/ManagerChecker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManagerChecker : MonoBehaviour {
+    public GameObject stageManagerPrefab;
+   
+
+	// Use this for initialization
+	void Start () {
+        GameObject Manager = GameObject.Find("StageManager");
+	    if(Manager != null)
+        {
+            Destroy(Manager);
+            Debug.Log("Destroyed");
+        }
+        Instantiate(stageManagerPrefab);
+        Debug.Log("Generated");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Project/Assets/Scripts/Title/NumStage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumStage : MonoBehaviour {
+
+    public static int numberOfStage = 1;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

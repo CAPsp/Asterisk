30c54b66bfcdfa86f87428c11b8d1e8f4f70eb39
Masaya Kobayashi <kobayashi03@hotmail.co.jp>
2016-09-25 04:18:10 +0900

    [add]矢が当たった地点の記憶と矢の軌跡を表示

---
+++ Project/Assets/Scripts/Game/HitPointManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HitPointManager : MonoBehaviour{
+
+	List<Vector2> mHitPoints;
+
+	public void Awake(){
+		mHitPoints = new List<Vector2> ();
+	}
+
+	public void AddHitPoint(Vector2 item){
+		mHitPoints.Add (item);
+	}
+
+	public List<Vector2> GetHitPointList(){
+		return mHitPoints;
+	}
+
+}
--- Project/Assets/Scripts/Game/Sucker.cs
+++ Project/Assets/Scripts/Game/Sucker.cs
@@ -1,12 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using Original.Util;
 
 public class Sucker : MonoBehaviour {
-	
+
+	[SerializeField] GameObject mPrefabLine;
+
 	Rigidbody2D mRigidbody;
+	HitPointManager mHitPointManager;
+	List<GameObject> mLineList;
 
 	void Awake(){
-		mRigidbody = GetComponent<Rigidbody2D>();
+		mRigidbody 			= GetComponent<Rigidbody2D>();
+		mHitPointManager 	= GetComponent<HitPointManager>();
+
+		mLineList  = new List<GameObject>();
+		GameObject newPrefab = Instantiate (mPrefabLine, Vector3.zero, Quaternion.identity) as GameObject;
+		newPrefab.GetComponent<LineRenderer> ().SetPosition (0, transform.position + Vector3.back);
+		mLineList.Add (newPrefab);
+	}
+
+	void Update(){
+
+		// 当たった場所があったら線を引く。(そのためには前に当たった場所を順に追跡する)
+		List<Vector2> tmp = mHitPointManager.GetHitPointList();
+		for(int i = 0; i < tmp.Count; i++){
+
+			if (i + 2 > mLineList.Count) {
+				mLineList [mLineList.Count - 1].GetComponent<LineRenderer>().SetPosition (1, new Vector3(tmp[i].x, 	tmp[i].y,	-1));
+
+				GameObject newPrefab = Instantiate (mPrefabLine, Vector3.zero, Quaternion.identity) as GameObject;
+				newPrefab.GetComponent<LineRenderer> ().SetPosition (0, new Vector3(tmp[i].x, 	tmp[i].y,	-1));
+
+				mLineList.Add (newPrefab);
+			}
+
+		}
+
+		// 最後に当たった場所からSuckerへの線を引く
+		mLineList[mLineList.Count - 1].GetComponent<LineRenderer>().SetPosition(1, transform.position + Vector3.back);
+
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
@@ -19,6 +53,8 @@ public class Sucker : MonoBehaviour {
 		else if(other.transform.parent.tag == "GameAreaY"){
 			mRigidbody.velocity = new Vector2(vx, vy * (-1));
 		}
+
+		mHitPointManager.AddHitPoint (mRigidbody.transform.position);
 
 	}
 

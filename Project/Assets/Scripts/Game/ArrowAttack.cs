e9a1aed10d07f2c823f24151357918119e789e99
Masaya Kobayashi <kobayashi03@hotmail.co.jp>
2016-09-24 19:06:56 +0900

    [add]角度に制限追加

--- Project/Assets/Scripts/Game/ArrowAttack.cs
+++ Project/Assets/Scripts/Game/ArrowAttack.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class ArrowAttack : MonoBehaviour {
 
+	[SerializeField] float mAngleLimit = 60f;
 
 	ArrowMovement mArrowMovement;
 
@@ -18,14 +19,15 @@ public class ArrowAttack : MonoBehaviour {
 
 		// debug
 		if (Input.GetKey ("mouse 0")) {
-			Vector3 diff = PointaToPosition.ChangeToPostion(Input.mousePosition) - transform.position;
+			Vector3 diff = PointaToPosition.ChangeToPostion (Input.mousePosition) - transform.position;
 
 			float angle = Mathf.Atan2 (diff.x, diff.y);
 			angle = Mathf.Rad2Deg * angle;	// rad→角度
+			angle = Mathf.Clamp (angle, (-1) * mAngleLimit, mAngleLimit);
 
-			Quaternion targetQua = Quaternion.AngleAxis (angle, Vector3.forward);
-
-			Debug.Log ("angle = " + angle);
+			transform.rotation = Quaternion.AngleAxis (angle, Vector3.back);
+		}
+		else if(Input.GetKeyUp("mouse 0")){
 
 		}
 

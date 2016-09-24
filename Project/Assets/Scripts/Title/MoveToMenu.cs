3236413a46ec04bd7636bb221fe24254fc962c3d
Hokoyama <k0416m0204@yahoo.co.jp>
2016-09-24 17:53:57 +0900

    Titleシーンからのフェードアウト、シーン以降の実装

---
+++ Project/Assets/Scripts/Title/MoveToMenu.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MoveToMenu : MonoBehaviour {
+    public float fadeTime = 1.0f;
+    private bool changeToMenu = false;
+    private float currentRemainTime;
+    private float red, green, blue;
+
+	// Use this for initialization
+	void Start () {
+        currentRemainTime = fadeTime;
+        red = GetComponent<Image>().color.r;
+        green = GetComponent<Image>().color.g;
+        blue = GetComponent<Image>().color.b;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            changeToMenu = true;
+        }
+
+        if (changeToMenu == true)
+        {
+            currentRemainTime -= Time.deltaTime;
+
+            if(currentRemainTime <= 0.0f)
+            {
+                SceneManager.LoadScene("Menu");
+                return;
+            }
+
+            float alpha = currentRemainTime / fadeTime;
+            alpha = 1 - alpha;
+            GetComponent<Image>().color = new Color(red, green, blue, alpha);
+        }
+
+    }
+}

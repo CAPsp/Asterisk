2c82b192c000d854bb2b41696a74c7f8b624eaff
Masaya Kobayashi <kobayashi03@hotmail.co.jp>
2016-09-25 02:58:21 +0900

    [add]ガイドラインの追加

--- Project/Assets/Scripts/Game/ArrowAttack.cs
+++ Project/Assets/Scripts/Game/ArrowAttack.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using Original.Util;
 
 public class ArrowAttack : MonoBehaviour {
 
@@ -7,12 +8,15 @@ public class ArrowAttack : MonoBehaviour {
 	[SerializeField] GameObject mPrefabSucker;
 	[SerializeField] float mSpeed = 10.0f;
 	[SerializeField] Transform mTargetTrans;
+	[SerializeField] GameObject mObjectIncludeLine;
 
 	ArrowMovement mArrowMovement;
 	Vector3 mPosition;
+	LineRenderer mFirstLine;
 
 	void Awake(){
-		mArrowMovement = GetComponent<ArrowMovement> ();
+		mArrowMovement 	= GetComponent<ArrowMovement> ();
+		mFirstLine		= GetComponent<LineRenderer>();
 	}
 
 	void Start(){
@@ -23,23 +27,73 @@ public class ArrowAttack : MonoBehaviour {
 
 		// debug
 		if (Input.GetKey ("mouse 0")) {
-			Vector3 diff = PointaToPosition.ChangeToPostion (Input.mousePosition) - transform.position;
-
-			float angle = Mathf.Atan2 (diff.x, diff.y);
-			angle = Mathf.Rad2Deg * angle;	// rad→角度
-			angle = Mathf.Clamp (angle, (-1) * mAngleLimit, mAngleLimit);
-
-			transform.rotation = Quaternion.AngleAxis (angle, Vector3.back);
+			Preparation ();
 		}
 		else if(Input.GetKeyUp("mouse 0")){
-			Transform transform = (gameObject.GetComponentsInChildren<Transform>())[1];
-			GameObject sucker 	= Instantiate (mPrefabSucker, transform.position, Quaternion.identity) as GameObject;
-
-			Vector3 diff = (mTargetTrans.position - transform.position);
-			diff = new Vector3 (diff.x, diff.y, 0f);
-			sucker.GetComponent<Rigidbody2D> ().velocity = mSpeed * diff.normalized;
+			Fire ();
 		}
 
+	}
+
+	void Preparation(){
+
+		mFirstLine.enabled = true;
+
+		// 角度をつける
+		Vector3 diff = PointaToPosition.ChangeToPostion (Input.mousePosition) - transform.position;
+
+		float angle = Mathf.Atan2 (diff.x, diff.y);
+		angle = Mathf.Rad2Deg * angle;	// rad→角度
+		angle = Mathf.Clamp (angle, (-1) * mAngleLimit, mAngleLimit);
+
+		transform.rotation = Quaternion.AngleAxis (angle, Vector3.back);
+	
+		// ガイドラインが反射する可能性を考慮
+		Vector2 vec2Pos = new Vector2(transform.position.x, transform.position.y);
+		RaycastHit2D hit = 
+			Physics2D.Raycast (	vec2Pos,
+								Vec3ToVec2.GenVecFrom2Points (mTargetTrans.position, transform.position).normalized,
+								Vec3ToVec2.CalcDistanceOn2D (transform.position, mTargetTrans.position));
+
+		// 反射したかを考えてガイドラインを表示
+		if (hit.collider != null) {
+
+			// 一本目の線
+			Vector3 middle = new Vector3(hit.point.x, hit.point.y);
+			mFirstLine.SetPosition (0, transform.position + Vector3.back);
+			mFirstLine.SetPosition (1, middle + Vector3.back);
+
+			// 二本目の線
+			float remainLength = Vec3ToVec2.CalcDistanceOn2D (transform.position, mTargetTrans.position)
+									- Vec3ToVec2.CalcDistanceOn2D (transform.position, middle);
+
+			Vector2 tmp = Vec3ToVec2.GenVecFrom2Points (mTargetTrans.position, transform.position).normalized;
+			Vector3 reflectAngle = new Vector3(tmp.x * (-1), tmp.y, 0);
+
+			mObjectIncludeLine.SetActive(true);
+			mObjectIncludeLine.GetComponent<LineRenderer> ().SetPosition (0, middle + Vector3.back);
+			mObjectIncludeLine.GetComponent<LineRenderer> ().SetPosition (1, middle + reflectAngle.normalized * remainLength);
+			
+		}
+		else {
+			mFirstLine.SetPosition (0, transform.position + Vector3.back);
+			mFirstLine.SetPosition (1, mTargetTrans.position + Vector3.back);
+			mObjectIncludeLine.SetActive(false);
+		}
+
+	}
+
+	void Fire(){
+
+		mFirstLine.enabled = false;
+		mObjectIncludeLine.SetActive(false);
+
+		Transform transform = (gameObject.GetComponentsInChildren<Transform>())[1];
+		GameObject sucker 	= Instantiate (mPrefabSucker, transform.position, Quaternion.identity) as GameObject;
+
+		Vector3 diff = (mTargetTrans.position - transform.position);
+		diff = new Vector3 (diff.x, diff.y, 0f);
+		sucker.GetComponent<Rigidbody2D> ().velocity = mSpeed * diff.normalized;
 	}
 
 }
--- Project/Assets/Scripts/Game/Sucker.cs
+++ Project/Assets/Scripts/Game/Sucker.cs
@@ -2,26 +2,21 @@
 using System.Collections;
 
 public class Sucker : MonoBehaviour {
-
-	Collider2D mColliderAreaX;
-	Collider2D mColliderAreaY;
+	
 	Rigidbody2D mRigidbody;
 
 	void Awake(){
 		mRigidbody = GetComponent<Rigidbody2D>();
-		GameObject gameArea = GameObject.FindWithTag("GameArea");
-		mColliderAreaX = (gameArea.GetComponentsInChildren<Collider2D>())[0];
-		mColliderAreaY = (gameArea.GetComponentsInChildren<Collider2D>())[1];
 	}
 
-	void OnTriggerExit2D(Collider2D other){
+	void OnTriggerEnter2D(Collider2D other){
 
 		float vx = mRigidbody.velocity.x;
 		float vy = mRigidbody.velocity.y;
-		if (other == mColliderAreaX) {
+		if (other.transform.parent.tag == "GameAreaX") {
 			mRigidbody.velocity = new Vector2(vx * (-1), vy);
 		}
-		else if(other == mColliderAreaY){
+		else if(other.transform.parent.tag == "GameAreaY"){
 			mRigidbody.velocity = new Vector2(vx, vy * (-1));
 		}
 

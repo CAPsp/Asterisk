7088d31d3ad2be80b7bd8ddf1e1fd135b8e911ed
Hokoyama <k0416m0204@yahoo.co.jp>
2016-09-25 09:21:19 +0900

    Menuシーンからのボタンで各シーンへの移行の実装

---
+++ Project/Assets/Scripts/Title/test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Debug Start");
+        //ストリーミングアセットのパス
+        Debug.Log("Streaming Path" + Application.streamingAssetsPath);
+        //Unityが利用するデータが保存されるパス
+        Debug.Log("Data Path" + Application.dataPath);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
